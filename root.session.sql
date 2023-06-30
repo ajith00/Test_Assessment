4d49aec3d605f828f3fd4c852a73ae90dd09bd31
ajith00 <ajithvenkatesh129@gmail.com>
2023-06-30 20:43:18 +0530

    added a db Backup file

--- root.session.sql
+++
@@ -1,6 +0,0 @@
-SELECT * from userlogin;
-SELECT * from responces;
-SELECT * from assessments;
-delete from responces where responces.idresponces=5;
-ALTER TABLE responces rename column assesmentid to AssessmentID;
-ALTER TABLE assessments rename column assementName to AssessmentName;
